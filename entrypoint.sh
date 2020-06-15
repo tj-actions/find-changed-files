47b8faa48966209ebbacf3aeb30f1d5efefd74e9
Tonye Jack <jtonye@ymail.com>
2020-06-15 05:40:21 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,19 @@
 
 set -e
 
+GITHUB_TOKEN=$1
+FILES=$2
 
-echo "::set-output name=files_changed::true"
+echo "Formatting filenames..."
+EXPECTED_FILES="$(printf $(echo ${FILES} | sed 's| ||g' | sed 's/,/|/g'))"
+
+echo "Checking changes for \"${EXPECTED_FILES}\"... "
+CHANGED_FILES=$(git diff --diff-filter=ACM --name-only | grep -E "(${EXPECTED_FILES})" || true)
+
+if [[ -z ${CHANGED_FILES} ]]; then
+  echo "::set-output name=files_changed::false"
+  exit 0;
+else
+  echo "::set-output name=files_changed::true"
+  exit 0;
+fi
